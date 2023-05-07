1813d3c307d78279f3e12890d55f8d88d101c456
Joshua Tafoya <tafoyajd.91@gmail.com>
2023-05-06 21:22:27 -0700

    fruitloops

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = []
+const messages = []
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    };
+    return messages;
+}
+
+function countDown(i) {
+    while (i > -1) {
+        console.log(i);
+        i--;
+    }
+}
+
